784a9a90a0423b7a64ff1ebed79d5af13f1f56e2
brad@smithwebtek.com <brad@smithwebtek.com>
2021-02-28 16:38:57 -0500

    Work on views

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,13 +1,14 @@
 class PatientsController < ApplicationController
-
   def index
-    @patients = Patient.all
-    render json: @patients
+    @patients = Patient.all.first(5)
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @patients }
+    end
   end
 
   def show
     @patient = Patient.find(params[:id])
     render json: @patient
   end
-
 end
