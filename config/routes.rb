e6056c5669a38595576ec63ef3f91449aa833140
brad@smithwebtek.com <brad@smithwebtek.com>
2021-02-27 21:54:46 -0500

    Add highcharts example

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :patients
+  get 'people', to: 'static#people'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
